d3fd2b6de5a983932fac79de72fcfb994b9970fd
naba93980 <72063612+naba93980@users.noreply.github.com>
2024-02-04 18:49:54 +0530

    spring security basic - version 2

---
+++ spring_security_javabrains/src/main/java/com/jbrains/springsecurityjbrains/SpringsecurityjbrainsApplication.java
@@ -0,0 +1,13 @@
+package com.jbrains.springsecurityjbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityjbrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityjbrainsApplication.class, args);
+	}
+
+}
